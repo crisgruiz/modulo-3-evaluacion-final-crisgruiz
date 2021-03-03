0e2673fac627896bef81ffa148c984c3474b3165
Cristina Gonzalez <crisgruiz@protonmail.com>
2021-03-03 17:14:03 +0100

    Create repository

---
+++ evaluacion-final-modulo3/src/components/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "../styleSheets/App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code></code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
